82d763f9718b9669033cc90e1477e25d2a686c4e
quedes <knecht1989@aol.com>
2015-12-16 12:54:56 +0100

    Update details.php

--- details.php
+++ details.php
@@ -12,12 +12,12 @@ else
 
 /* Verbindung zum Server*/
 		
-$db = mysql_connect("","","");
+$db = mysql_connect("localhost:3306","web275","webhostingpartei42");
 mysql_set_charset("utf8", $db);
 if($db)
 {
-	
-	mysql_select_db("");
+	/*mysql_select_db("web275_db1");*/
+	mysql_select_db("web275_db1");
 	
 	$abfrage = "SELECT * FROM haka_wohnen WHERE ind = '$pam'";
 	$ergebnis = mysql_query($abfrage);
@@ -34,7 +34,7 @@ if($db)
 	/*Bilder für Slideshow*/
 	$pic = $list[0] ["wohnung"];
 	
-	$picone = "pic/" . $pic . "_one" . ".png";
+	$picone = "img/" . $pic . ".jpg";
 	
 	echo "<div>";
 		if (file_exists($picone))
@@ -80,20 +80,29 @@ if($db)
 	$austattung_boden = $list[0] ["boden"];
 	
 	echo "<table class=\"table_unten\">";
+		if($austattung_kueche !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Küche</th>";
+			echo "<th>Küche</th>";
 			echo "<td>$austattung_kueche</td>";
 		echo "</tr>";
+		}
 		
+		if($austattung_bad !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Bad</th>";
+			echo "<th>Bad</th>";
 			echo "<td>$austattung_bad</td>";
 		echo "</tr>";
+		}
 		
+		if($austattung_boden !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Boden</th>";
+			echo "<th>Boden</th>";
 			echo "<td>$austattung_boden</td>";
 		echo "</tr>";
+		}
 	echo "</table>";
 	
 	/*Energieausweis,Heizungsart,Restaurierung,*/
@@ -102,65 +111,92 @@ if($db)
 	$restaurierung = $list[0] ["restaurierung"];
 	
 	echo "<table class=\"table_unten\">";
-	/*echo "<caption>Zustand</caption>";*/
+		if($energie !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Energieausweis</th>";
+			echo "<th>Energieausweis</th>";
 			echo "<td>$energie</td>";
 		echo "</tr>";
+		}
 		
+		if($heizung !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Heizung</th>";
+			echo "<th>Heizung</th>";
 			echo "<td>$heizung</td>";
 		echo "</tr>";
+		}
 		
+		if($restaurierung !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Restaurierung</th>";
+			echo "<th>Restaurierung</th>";
 			echo "<td>$restaurierung</td>";
 		echo "</tr>";
+		}
 	echo "</table>";
 	
 	
 	/*Kalt,Warm,Kaution*/
-	$kalte = $list[0] ["kalt"];
+	$kalt = $list[0] ["kalt"];
 	$warm = $list[0] ["warm"];
 	$kaution = $list[0] ["kaution"];
 	
 	echo "<table class=\"table_unten\">";
+		if($kalt !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Kaltmiete</th>";
-			echo "<td>$kalte</td>";
+			echo "<th>Kaltmiete</th>";
+			echo "<td>$kalt</td>";
 		echo "</tr>";
+		}
 		
+		if($warm !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Nebenkosten</th>";
+			echo "<th>Warmmiete</th>";
 			echo "<td>$warm</td>";
 		echo "</tr>";
+		}
 		
+		if($kaution !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Kaution</th>";
+			echo "<th>Kaution</th>";
 			echo "<td>$kaution</td>";
 		echo "</tr>";
+		}
 	echo "</table>";
 	/*etage, garage*/
 	
 	$etage = $list[0] ["etage"];
 	$garage = $list[0] ["garage"];
+	$sonstiges = $list[0] ["sonstiges"];
 	
 	echo "<table class=\"table_unten\">";
+		if($etage !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Etage</th>";
+			echo "<th>Etage</th>";
 			echo "<td>$etage</td>";
 		echo "</tr>";
+		}
 		
+		if($garage !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Garage/Stellplatz</th>";
+			echo "<th>Garage</th>";
 			echo "<td>$garage</td>";
 		echo "</tr>";
+		}
 		
+		if($sonstiges !='')
+		{
 		echo "<tr>";
-			echo "<th class=\"th_unten\">Sonstiges</th>";
-			echo "<td></td>";
+			echo "<th>Sonstiges</th>";
+			echo "<td>$sonstiges</td>";
 		echo "</tr>";
+		}
 	echo "</table>";
 
 	echo "<form action=\"main_haka_kontakt.html\" method=\"POST\">";
