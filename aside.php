7c0cbee1856709cce5cb2f00fa12fc5a2324d1a0
quedes <knecht1989@aol.com>
2015-12-16 16:40:54 +0100

    Update aside.php

--- aside.php
+++ aside.php
@@ -7,25 +7,25 @@
 	
 	<div id='menue'>
 		<div style='padding-bottom:100%; padding-top:5%;'>
-			<a href='main_haka.html'>
+			<a href='main_haka.php'>
 			<div style='background-color:black; color: white;' class='menue_punkte'>
 				Home
 			</div>
 			</a>
 		
-			<a href='main_haka_angebote.html'>
+			<a href='main_haka_angebote.php'>
 			<div style='background-color:white; color: black;' class='menue_punkte'>
 				Angebote
 			</div>
 			</a>
 		
-			<a href='main_haka_kontakt.html'>
+			<a href='main_haka_kontakt.php'>
 			<div   style='background-color:white; color: black;' class='menue_punkte'>
 				Kontakt
 			</div>
 			</a>
 			
-			<a href='main_haka_impressum.html'>
+			<a href='main_haka_impressum.php'>
 			<div style='background-color:orange; color: black;' class='menue_punkte'>
 				Impressum
 			</div>
