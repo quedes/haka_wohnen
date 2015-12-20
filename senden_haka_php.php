6879473f3e905dd7664aa093b3ffe200ba488022
quedes <knecht1989@aol.com>
2015-12-20 16:34:19 +0100

    Update senden_haka_php.php

--- senden_haka_php.php
+++ senden_haka_php.php
@@ -1,31 +1,6 @@
 <?php
 header("main_haka_kontakt.php");
 
-/*Funktion prüft Korrekte Struktur der email-adresse*/
-function check_email($email) {
-  
-  $nonascii      = "\x80-\xff"; # Non-ASCII-Chars are not allowed
-
-  $nqtext        = "[^\\\\$nonascii\015\012\"]";
-  $qchar         = "\\\\[^$nonascii]";
-
-  $protocol      = '(?:mailto:)';
-
-  $normuser      = '[a-zA-Z0-9][a-zA-Z0-9_.-]*';
-  $quotedstring  = "\"(?:$nqtext|$qchar)+\"";
-  $user_part     = "(?:$normuser|$quotedstring)";
-
-  $dom_mainpart  = '[a-zA-Z0-9][a-zA-Z0-9._-]*\\.';
-  $dom_subpart   = '(?:[a-zA-Z0-9][a-zA-Z0-9._-]*\\.)*';
-  $dom_tldpart   = '[a-zA-Z]{2,5}';
-  $domain_part   = "$dom_subpart$dom_mainpart$dom_tldpart";
-
-  $regex         = "$protocol?$user_part\@$domain_part";
- 
-
-  return preg_match("/^$regex$/",$email);
-}
-
 
 
 
@@ -45,7 +20,7 @@ $text = $_POST['nachricht'];
 $text = htmlentities($text);
 
 /*Prüft DNS Einträge auf Übereinstimmung mit IP-Adresse*/
- if(checkdnsrr($from,'MX') && check_email($from))
+ if(checkdnsrr($from,'MX') && filter_var($from, FILTER_VALIDATE_EMAIL))
  {
 	mail($empfaenger, $betreff, $text, $from);
  }
